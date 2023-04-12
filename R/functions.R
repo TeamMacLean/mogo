9d8f986a011f1dbded140f7e5ee63abce25a3e12
Dan MacLean <maclean.daniel@gmail.com>
2023-04-12 13:41:56 +0100

    fix data use

--- R/functions.R
+++ R/functions.R
@@ -1,15 +1,24 @@
 
 #' get all known M.oryzae gene ids - the universe
 #' @param genefile csv file with GeneID column of all Mo ids
-all_mo_genes <- function(genefile=here::here("inst", "extdata", "all_mo_ids.csv")){
-  readr::read_csv(genefile)$GeneID
+all_mo_genes <- function(genefile=NA){
+  #here::here("inst", "extdata", "all_mo_ids.csv")
+  if (! is.na(genefile)){
+    readr::read_csv(genefile)$GeneID
+  }
+  else {
+    mo_gene_ids
+  }
 }
 
 #' get mapping between Mo Gene IDs and GO Terms
 #' @param termsfile TSV file from biomart export of GO terms
-mo_mapping <- function(termsfile=here::here("inst", "extdata", "MG8_mart_export.txt")) {
-  terms <- readr::read_tsv(termsfile)
-
+mo_mapping <- function(termsfile=NA) {
+  #here::here("inst", "extdata", "MG8_mart_export.txt")
+  terms <- terms_data
+  if (! is.na(termsfile)){
+    terms <- readr::read_tsv(termsfile)
+  }
   term2gene <- data.frame(
     term = terms$`GO term accession`,
     gene = terms$`Gene stable ID`
@@ -34,8 +43,13 @@ mo_mapping <- function(termsfile=here::here("inst", "extdata", "MG8_mart_export.
 #' @param termsfile TSV file from biomart export of GO terms
 #' @return enricher object
 #' @export
-do_enrich <- function(genes, termsfile=here::here("inst", "extdata", "MG8_mart_export.txt"), ...) {
-  info <- mo_mapping(termsfile)
+do_enrich <- function(genes, termsfile=NA, ...) {
+
+  here::here("inst", "extdata", "MG8_mart_export.txt")
+  info <- mo_mapping()
+  if (! is.na(termsfile)){
+    info <- mo_mapping(termsfile=termsfile)
+  }
   clusterProfiler::enricher( genes,
                              universe=info$all_genes,
                              TERM2GENE=info$term2gene,
@@ -50,10 +64,13 @@ do_enrich <- function(genes, termsfile=here::here("inst", "extdata", "MG8_mart_e
 #' @param termsfile TSV file from biomart export of GO terms
 #' @return DAVID format dataframe
 #' @export
-enricher_to_david <- function(enrich,termsfile=here::here("inst","extdata", "MG8_mart_export.txt")){
+enricher_to_david <- function(enrich,termsfile=NA){
   comma_sep_genes = gsub("/", ", ", enrich@result$geneID)
+  #here::here("inst","extdata", "MG8_mart_export.txt")
+  terms <- terms_data
+  if (!is.na(termsfile)){
+    terms <- readr::read_tsv(termsfile)
 
-  terms <- readr::read_tsv(termsfile)
   terms <- dplyr::mutate(terms,
     short_category = dplyr::if_else(`GO domain` == 'biological_process', "BP",
                                     dplyr::if_else(`GO domain` == "molecular_function", "MF", "CC"))
